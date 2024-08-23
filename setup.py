f8cb8f5bec7dfc873cb958bc7ba65c4378c57e0b
XCWQW233 <3539757707@qq.com>
2024-08-24 07:56:15 +0800

    更改自动构建脚本

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-import os
 import subprocess
-import sys
-
-main_script = 'main.py'
-
-files_and_folders = [
-    'app/*',
-]
 
 # 生成Nuitka的命令
 nuitka_command = [
@@ -21,20 +13,9 @@ nuitka_command = [
     '--assume-yes-for-downloads',
     '--windows-disable-console',
     '--onefile',
+    '--include-data-dir=app=app'
 ]
 
-# 添加数据文件和文件夹
-for item in files_and_folders:
-    abs_path = os.path.abspath(item)
-    if os.path.isdir(abs_path):
-        nuitka_command.extend([
-            f'--include-data-dir={abs_path}={item}'
-        ])
-    else:
-        nuitka_command.extend([
-            f'--include-data-file={abs_path}={item}'
-        ])
-
 print(nuitka_command)
-nuitka_command.append(main_script)
+nuitka_command.append('main.py')
 subprocess.run(nuitka_command)
